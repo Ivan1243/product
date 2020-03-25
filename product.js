53ca89c918466bddcf7421db8247e4b6416da107
Ivan <ostankov-ii@mail.ru>
2020-03-25 20:27:48 +0300

    first commit

---
+++ product.js
@@ -0,0 +1,18 @@
+class Product {
+    constructor(name, count){
+        this.name = name;
+        this.count = count;
+    }
+    sell(count){
+        if (this.count < count){
+            console.log("Нет в наличии");
+        }
+        else{
+            this.count -= count
+            console.log("Товар " + this.name + " продан в количестве " + count.toString());
+            console.log("Оставшееся количество " + this.count.toString());
+        }            
+    }
+}
+products = [new Product("apples", 10), new Product("pears", 12)];
+console.log(Product(1));
